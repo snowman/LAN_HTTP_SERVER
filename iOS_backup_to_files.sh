0d225d4c0b1d38f3346f0022cce49775828ff9b3
snowman <unmem@outlook.com>
2021-10-10 11:40:13 +0800

    update

--- iOS_backup_to_files.sh
+++ iOS_backup_to_files.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Usage:
+#
+# Export notes by using latest first in Shortcut app
+
 # First time
 #
 # $ mv -iv PASTE_BIN notes/01.txt
@@ -9,24 +13,25 @@
 #
 # ... time
 # $ mv -iv PASTE_BIN notes/NN.txt
+#
+# Lastest notes has smaller number, oldest one has greater number
 
 # The "Shortcut" app only allow select FIRST 26 notes at a time.
-# (sort then select, so each time the order is remain unchanged.)
+# (NOTICE: it's select first, then sort)
 
-# Think oldest has smaller number, lastest has greater number, like (1, 2, 3, ..., N) etc
-
-# The export notes is oldest first
+# Explode notes to single text file one by one by separator
 find notes -name "*.txt" | sort -n |\
 xargs awk 'FNR==1 {print "@@@@,@@@@"} { print }' | sed '1d' |\
 sed 's/\r$//' |\
 awk 'BEGIN { RS="\n@@@@,@@@@\n" }
-           { print $0 > "output/" sprintf("%03d", NR) }'
+           { a[i++]=$0 }
+     END   {
+              while(i--)
+                print a[i] > "output/" sprintf("%03d", NR - i)
+           }'
 
-# If the file is lastest first, the code below reverse order after read all.
+# Run below when the export notes is oldest first, but that's NOT possible
+# Because "Shortcut" app select notes first then sort
 #
 # awk 'BEGIN { RS="\n@@@@,@@@@\n" }
-#            { a[i++]=$0 }
-#      END   {
-#              while(i--)
-#                print a[i] > "output/" sprintf("%03d", NR - i)
-#            }'
+#            { print $0 > "output/" sprintf("%03d", NR) }'
