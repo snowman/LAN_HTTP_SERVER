7fa66bfb1b9cbacb81aac35f46d8cbae3360e4aa
snowman <unmem@outlook.com>
2021-10-10 20:44:38 +0800

    update

--- read_notes.py
+++ read_notes.py
@@ -2,6 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import re
+import os
+
+dest = "notes"
+if os.path.exists(dest) and os.path.isdir(dest):
+    if not os.listdir(dest):
+        print("Directory is empty")
+    else:
+        print("Directory is not empty")
+        import sys
+        sys.exit(1)
 
 with open("PASTE_BIN", "r", encoding='utf-8') as f:
     text = ''.join(f.readlines()[1:]).strip()
@@ -12,5 +22,5 @@ with open("PASTE_BIN", "r", encoding='utf-8') as f:
         space = ("  " if ((idx+1) % 25 == 0 and (idx+1) % 50 != 0) else "")
         number_newline = (str(idx+1).rjust(4, ' ') + "\n" if idx !=0 and (idx+1) % 50 == 0 else "")
         print("." + space + number_newline, end='')
-        filename = "notes\\" + str(idx+1).rjust(3, '0')
+        filename = os.path.join(dest, str(idx+1).rjust(3, '0'))
         print(note.strip(), file=open(filename, "w", encoding='utf-8'))
